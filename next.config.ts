386e1155f0fb7fb6a25ffb7e94a3dd0c0b9cbd6f
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-11-29 22:33:48 +0100

    refactor(next.config): simplify configuration by removing GitHub Pages logic

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProjectPages = process.env.GITHUB_PAGES === 'true';
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
-
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: isProjectPages && repoName ? `/${repoName}` : '',
-  assetPrefix: isProjectPages && repoName ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
