b5f9989f4dd33dd32766022cb339e1dd418b375b
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-11-29 22:48:49 +0100

    feat(next.config): add redirects to default configuration for French localization

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/fr/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
