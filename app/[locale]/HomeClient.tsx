ca5cab63b2fa30efbb501e50b43c8a05f0297ed2
eliottscherrer <146830321+eliottscherrer@users.noreply.github.com>
2025-11-30 00:25:59 +0100

    fix(HomeClient): replace JavaScript tech logo with React logo in frontend tech stack

--- app/[locale]/HomeClient.tsx
+++ app/[locale]/HomeClient.tsx
@@ -33,6 +33,7 @@ import {
   SiBlazor,
   SiMysql,
   SiNextdotjs,
+  SiReact,
 } from 'react-icons/si';
 import { DiDotnet } from "react-icons/di";
 import { useTranslations } from "next-intl";
@@ -42,7 +43,7 @@ import LanguageSwitcher from "@/components/LanguageSwitcher";
 const frontendTech = [
   { node: <TechLogo icon={<SiHtml5 />} label="HTML" />, title: "HTML5", href: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
   { node: <TechLogo icon={<SiCss3 />} label="CSS" />, title: "CSS3", href: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
-  { node: <TechLogo icon={<SiJavascript />} label="JavaScript" />, title: "JavaScript", href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { node: <TechLogo icon={<SiReact />} label="React" />, title: "React", href: "https://reactjs.org" },
   { node: <TechLogo icon={<SiVuedotjs />} label="Vue.js" />, title: "Vue.js", href: "https://vuejs.org" },
   { node: <TechLogo icon={<DiDotnet />} label="C# .NET" />, title: "C# .NET", href: "https://docs.microsoft.com/en-us/dotnet/csharp/" },
   { node: <TechLogo icon={<SiNextdotjs />} label="Next.js" />, title: "Next.js", href: "https://nextjs.org" },
